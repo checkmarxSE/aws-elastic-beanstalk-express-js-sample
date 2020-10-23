5d9afbab685a2f1350e73f3a6bae015bb926f0a0
checkmarxSE <60154583+checkmarxSE@users.noreply.github.com>
2020-10-22 23:35:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello AWS! Welcome to DevSecOps with Checkmarx!'));
+app.get('/', (req, res) => res.send('Hello AWS! Welcome to DevSecOps with Checkmarx! Incorporate best-in-breed SAST scanning with ease.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
